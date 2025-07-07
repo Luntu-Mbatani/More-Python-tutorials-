c983577ab69b12decac29d000007ed2f969a8330
Luntu Mbatani <LUNTUMBATANI26@GMAIL.COM>
2025-07-07 23:31:33 +0200

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,7 @@
+def area(length, width):
+    #function to calculate area
+    return length * width
+
+def perimeter(length, width):
+    #function to calculate perimeter
+    return 2 * (length + width)
